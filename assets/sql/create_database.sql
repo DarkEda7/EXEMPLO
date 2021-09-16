8d30657018cf037bafa5e629d8c0090c53fc14ff
DarkEda7 <edibmach@gmail.com>
2021-09-16 17:23:15 +0000

    preparação da base de dados

---
+++ assets/sql/create_database.sql
@@ -0,0 +1,12 @@
+create database biblioteca;
+use biblioteca;
+create table generos(
+    id int auto_increment primary key,
+    nome varchar(225) not null
+);
+create table livros (
+    id int auto_increment primary key,
+    titulo varchar(225) not null,
+    id_genero int not null,
+    foreign key (id_genero) references genero(id) 
+);
