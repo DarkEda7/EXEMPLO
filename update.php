b3ee6bc00babdd03bc1a88f0113708ca949f7d02
DarkEda7 <edibmach@gmail.com>
2021-09-27 18:00:39 +0000

    Salvar

---
+++ update.php
@@ -0,0 +1,39 @@
+<?php
+require_once './vendor/autoload.php';
+
+use ExemploPDOMySQL\MySQLconnection;
+
+$bd = new MySQLconnection();
+
+$genero = null;
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $comando = $bd->prepare('SELECT * FROM generos WHERE id = :id');
+    $comando->execute([':id' => $_GET['id']]);
+
+    $genero = $comando->fetch(PDO::FETCH_ASSOC);
+} else {
+    $comando = $bd->prepare('UPDATE generos SET nome = WHERE id = :id');
+    $comando->execute([':nome' => $_POST['nome'], ':id' => $_POST['id']]);
+
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Editar Gênero</title>
+    </head>
+    <body>
+        <h1>Editar Gênero</h1>
+        <form action="update.php" method="post">
+            <input type="hidden" name="id" value="<?= $genero['id'] ?>"
+            <label for ="nome">Nome do Gênero</label>
+            <input type="text" required name="nome" value="<?= $genero['id'] ?>"
+            <button type="submit">Salvar</button>
+        </form>
+    </body>
+    </html>
