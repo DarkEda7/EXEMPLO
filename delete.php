0ca20c1e8cb8b7316113821fb1b1d1a0ac5bb66d
DarkEda7 <edibmach@gmail.com>
2021-10-18 17:44:35 +0000

    salvar

--- delete.php
+++ delete.php
@@ -26,13 +26,14 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     <head>
         <meta charset="utf-8">
         <title>Remover Gênero</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
     </head>
     <body>
         <h1>Remover Gênero</h1>
         <p>Tem certeza que deseja remover o gênero "<?= $genero['nome'] ?>" ?</p>
         <form action="delete.php" method="post">
             <input type="hidden" name="id" value="<?= $genero['id'] ?>" />
-            <button type="submit">Excluir</button>
+            <button class="btn btn-success" type="submit">Excluir</button>
         </form>
     </body>
 </html>
