a652b6089b822148dae8227c4033155527962cda
DarkEda7 <edibmach@gmail.com>
2021-10-20 12:06:51 +0000

    Salvar

--- update.php
+++ update.php
@@ -19,22 +19,22 @@ if($_SERVER['REQUEST_METHOD'] == 'GET') {
     header('Location:/index.php');
 }
 
+$_title = 'Editar Gênero';
+
 ?>
 
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="utf-8">
-        <title>Editar Gênero</title>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-    </head>
-    <body>
+<?php include(' ./includes/header.php') ?>
+
         <h1>Editar Gênero</h1>
         <form action="update.php" method="post">
             <input type="hidden" name="id" value="<?= $genero['id'] ?>"
-            <label for ="nome">Nome do Gênero</label>
-            <input type="text" required name="nome" value="<?= $genero['nome'] ?>" />
+            <div class="form-group">
+                <label for ="nome">Nome do Gênero</label>
+                <input type="text" required name="nome" value="<?= $genero['nome'] ?>" />
+            </div>
+            <br />
+            <a class="btn btn-secondary" href="index.php">Voltar</a>
             <button class="btn btn-success" type="submit">Salvar</button>
         </form>
-    </body>
-    </html>
+
+<?php include(' ./includes/footer.php') ?>
